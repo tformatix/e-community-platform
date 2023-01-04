a228e32b21b2958aa9ce291db7c6ddc2eac95062
Michael Zauner <mzauner98@gmail.com>
2023-01-04 17:24:32 +0100

    Smart Contracts; bug fixes, added deposit/withDraw functions for payment between partners

--- in-progress/consent-management/src/blockchain/smart_contracts/contracts/ConsentContract.sol
+++ in-progress/consent-management/src/blockchain/smart_contracts/contracts/ConsentContract.sol
@@ -9,16 +9,35 @@ contract ConsentContractFactory {
     ConsentContract[] private contracts;
 
     // creates a new consent contract and returns its address
-    function createConsentContract() public returns(address) {
-        ConsentContract consentContract = new ConsentContract();
+    function createConsentContract(string memory _contractID) public {
+        ConsentContract consentContract = new ConsentContract(_contractID);
 
         contracts.push(consentContract);
+    }
 
-        return address(consentContract);
+    // returns address for given contractID
+    function getContractAddress(string memory _contractID) public view returns(address) {
+        for (uint i = 0; i < contracts.length; i++) {
+            string memory contractID = contracts[i].contractID();
+            if (keccak256(abi.encodePacked(contractID)) == keccak256(abi.encodePacked(_contractID))) {
+                return address(contracts[i]);
+            }
+        }
+
+        return address(contracts[0]);
     }
 }
 
 contract ConsentContract {
+
+    enum STATE {
+        CONTRACT_PENDING,
+        CONTRACT_SIGNED_BOTH,
+        CONTRACT_PAYMENT_RECEIVED,
+        CONTRACT_VALID,
+        CONTRACT_REJECTED,
+        CONTRACT_INVALID
+    }
 
     // both parties need to sign the contract
     struct ContractSign {
@@ -26,28 +45,32 @@ contract ConsentContract {
         bool signedByConsenter;
     }
 
-    // notify consenter that a contract got created
-    event ProposedContract();
+    // notify consenter that he received a payment => contract is now valid
+    event PaymentReceived(address proposer, address consenter, string contractID);
 
     // notify proposer that the contract got signed by the consenter
-    event ApprovedContract();
+    event ApprovedContract(address proposer, address consenter, string contractID);
 
     // ### Contract Details ###
-    
+    string public contractID;
     address private proposer; // address of the owner of the contract (proposer)    
     address private consenter; // address of the consenter
     uint private startEnergyData; 
     uint private endEnergyData;
-    uint private contractValidInDays;
+    uint private validityOfContract;
     uint private pricePerHour;
     uint private totalPrice;
     
     // signature of both parties
     ContractSign private signature;
 
+    // State of the contract
+    STATE private contractState;
+
     // set owner of the contract
-    constructor() {
+    constructor(string memory _contractID) {
         proposer = msg.sender;
+        contractID = _contractID;
     }
 
     // set details about the contract
@@ -55,14 +78,14 @@ contract ConsentContract {
         address _consenter, 
         uint _startEnergyData, 
         uint _endEnergyData,
-        uint _contractValidInDays,
+        uint _validityOfContract,
         uint _pricePerHour,
         uint _totalPrice
     ) public onlyOwner {
         consenter = _consenter;
         startEnergyData = _startEnergyData;
         endEnergyData = _endEnergyData;
-        contractValidInDays = _contractValidInDays;
+        validityOfContract = _validityOfContract;
         pricePerHour = _pricePerHour;
         totalPrice = _totalPrice;
     }
@@ -72,13 +95,17 @@ contract ConsentContract {
        can modify the regulations before the contract is active
     */
     function signContract() public bothParties {
-        if (msg.sender == proposer) { signature.signedByProposer = true; }
-        if (msg.sender == consenter) { signature.signedByConsenter = true; }
+        if (msg.sender == proposer) { 
+            signature.signedByProposer = true; 
+            contractState = STATE.CONTRACT_PENDING;
+        }
+        if (msg.sender == consenter) {
+            signature.signedByConsenter = true;
+        }
 
         if (signature.signedByProposer && signature.signedByConsenter) {
-            // TODO: calucalte contract validity time (contractValidInDays) upon both parties signed date
-            uint dat = block.timestamp + 3 minutes;
-            emit ApprovedContract();
+            emit ApprovedContract(proposer, consenter, contractID);
+            contractState = STATE.CONTRACT_SIGNED_BOTH;
         }
     }
 
@@ -86,16 +113,49 @@ contract ConsentContract {
     function revokeConsent() public bothParties {
     }
 
+    // proposer needs to deposit the agreed ethers to the contract
+    function deposit() public payable onlyOwner {
+        require(msg.value == totalPrice, "You need to deposit the agreed price!");
+    }
+
+    // get the balance of the contract
+    function balance() public view bothParties returns(uint) {
+        return address(this).balance;
+    }
+
+    // withdraw the ethers balance from the proposer
+    function withdraw() public onlyConsenter {
+        payable(consenter).transfer(address(this).balance);
+
+        emit PaymentReceived(consenter, proposer, contractID);
+
+        contractState = STATE.CONTRACT_VALID;
+    }
+
+    // returns the contract state
+    function getState() view public returns(STATE) {
+        if (contractState == STATE.CONTRACT_VALID) {
+            // check if contract is still valid
+        }
+        
+        return STATE.CONTRACT_INVALID;
+    }
 
     // modifier for functions only the owner can execute
     modifier onlyOwner() {
-        require(msg.sender == proposer);
+        //require(msg.sender == proposer);
+        _;
+    }
+
+    // modifier for functions only the consenter can execute
+    modifier onlyConsenter() {
+        //require(msg.sender == consenter);
         _;
     }
 
     // modifier for functions, both parties (proposer and consenter) can execute
     modifier bothParties() {
-        require(msg.sender == proposer || msg.sender == consenter);
+        //require(msg.sender == proposer || msg.sender == consenter);
         _;
     }
 }
--- src/e-community/backend/local/e-community-local/e-community-local/Util/Extensions/Python/goquorum.py
+++ src/e-community/backend/local/e-community-local/e-community-local/Util/Extensions/Python/goquorum.py
@@ -18,27 +18,31 @@ w3.handleRevert = True
 
 # CLI COMMANDS #
 # READ ETHEREUM ACCOUNT
-GOQUORUM_READ_ADDRESS = "cat /home/pi/blockchain/Node/data/keystore/accountAddress"
-GOQUORUM_READ_PASSWORD = "cat /home/pi/blockchain/Node/data/keystore/accountPassword"
-GOQUORUM_READ_PRIVATE_KEY = "cat /home/pi/blockchain/Node/data/keystore/accountPrivateKey"
+# GOQUORUM_READ_ADDRESS = "cat /home/pi/blockchain/Node/data/keystore/accountAddress"
+# GOQUORUM_READ_PASSWORD = "cat /home/pi/blockchain/Node/data/keystore/accountPassword"
+# GOQUORUM_READ_PRIVATE_KEY = "cat /home/pi/blockchain/Node/data/keystore/accountPrivateKey"
+
+GOQUORUM_READ_ADDRESS = "cat /home/michael/Documents/dev/network/QBFT-Network/Node-0/data/keystore/accountAddress"
+GOQUORUM_READ_PASSWORD = "cat /home/michael/Documents/dev/network/QBFT-Network/Node-0/data/keystore/accountPassword"
+GOQUORUM_READ_PRIVATE_KEY = "cat /home/michael/Documents/dev/network/QBFT-Network/Node-0/data/keystore/accountPrivateKey"
 
 # TRUFFLE #
 # deploy new consent contract
-TRUFFLE_DEPLOY_CONTRACT = 'cd ../../smart_contracts && truffle migrate | grep "contract ' \
+TRUFFLE_DEPLOY_CONTRACT = 'cd $SMART_CONTRACT_PATH && truffle migrate | grep "contract ' \
                           'address" | grep -o -E "0[xX][0-9a-fA-F]+"'
 
+TRUFFLE_CONTRACT_BYTECODE = 'cat $SMART_CONTRACT_PATH/build/contracts/ConsentContract.json | grep -oP \'(?<="bytecode": ")[^"]*\''
+TRUFFLE_READ_CONTRACT_ABI = 'cat $SMART_CONTRACT_PATH/build/contracts/ConsentContract.json'
 
-TRUFFLE_CONTRACT_BYTECODE = 'cat ../../smart_contracts/build/contracts/ConsentContract.json | grep -oP \'(?<="bytecode": ")[^"]*\''
-TRUFFLE_READ_CONTRACT_ABI = 'cat ../../smart_contracts/build/contracts/ConsentContract.json'
-
-TRUFFLE_CONTRACT_BYTECODE_FACTORY = 'cat ../../smart_contracts/build/contracts/ConsentContractFactory.json | grep -oP \'(?<="bytecode": ")[^"]*\''
-TRUFFLE_READ_CONTRACT_ABI_FACTORY = 'cat ../../smart_contracts/build/contracts/ConsentContractFactory.json'
+TRUFFLE_CONTRACT_BYTECODE_FACTORY = 'cat $SMART_CONTRACT_PATH/build/contracts/ConsentContractFactory.json | grep -oP \'(?<="bytecode": ")[^"]*\''
+TRUFFLE_READ_CONTRACT_ABI_FACTORY = 'cat $SMART_CONTRACT_PATH/build/contracts/ConsentContractFactory.json'
 
 # UNLOCK ETHEREUM ACCOUNT
 GOQUORUM_ACCOUNT_UNLOCK = 'curl -s -H "Content-Type: application/json" -X POST --data \'{"jsonrpc":"2.0",' \
                           '"method":"personal_unlockAccount","params":["%s", "%s"],"id":1}\' ' \
                           '%s'
 
+# GET ACCOUNT BALANCE
 GOQUORUM_ACCOUNT_BALANCE = 'curl -s -H "Content-Type: application/json" -X POST --data \'{"jsonrpc":"2.0",' \
                           '"method":"eth_getBalance","params":["%s", "latest"],"id":1}\' ' \
                           '%s'
@@ -58,10 +62,8 @@ class GoQuorumNode:
 
 
 @cli.command()
-def create_consent_contract(address_consenter: str = typer.Option(..., "--adr-con", "-ac", help="address of the consenter")):
+def deploy_consent_contract():
     """deploys a new consent contract to the blockchain and returns the deployed address"""
-
-    # account address Node: 0x7a1674007ef00a1179671acd7ac91379bffd8ba0
 
     if not w3.isConnected():
         print("Ethereum Node not running...")
@@ -88,11 +90,9 @@ def create_consent_contract(address_consenter: str = typer.Option(..., "--adr-co
     contract_bytecode = contract_bytecode_read.read().strip()
     contract_bytecode_hex = hex(int(contract_bytecode, 16))
 
-    # build checkSum from consenter's address
-    checksum_address_consenter = w3.toChecksumAddress(address_consenter)
-
     # create contract object and call constructor()
-    consent_contract_factory = w3.eth.contract(address=deployed_address, abi=contract_abi, bytecode=contract_bytecode_hex)
+    consent_contract_factory = w3.eth.contract(address=deployed_address, abi=contract_abi,
+                                               bytecode=contract_bytecode_hex)
 
     constructor_tx = consent_contract_factory.constructor().build_transaction(
         {
@@ -105,9 +105,31 @@ def create_consent_contract(address_consenter: str = typer.Option(..., "--adr-co
     # sign and make constructor transaction
     deployed_contract_factory_address = make_contract_tx(constructor_tx)
 
-    consent_contract_factory = w3.eth.contract(address=deployed_contract_factory_address, abi=contract_abi, bytecode=contract_bytecode_hex)
+    print(f'deployedAddress (factory): {deployed_contract_factory_address}')
 
-    create_consent_contract_tx = consent_contract_factory.functions.createConsentContract().build_transaction(
+
+@cli.command()
+def create_consent_contract(
+    address_contract_factory: str = typer.Option(..., "--adr-con-fac", "-acf", help="address of the contract factory"),
+    address_consenter: str = typer.Option(..., "--adr-con", "-ac", help="address of the consenter")
+):
+    if not w3.isConnected():
+        print("Ethereum Node not running...")
+        return
+
+    # read abi string from builded contract
+    contract_abi_read = os.popen(TRUFFLE_READ_CONTRACT_ABI_FACTORY)
+    contract_abi = json.loads(contract_abi_read.read())["abi"]
+
+    # extract the builded bytecode
+    contract_bytecode_read = os.popen(TRUFFLE_CONTRACT_BYTECODE_FACTORY)
+    contract_bytecode = contract_bytecode_read.read().strip()
+    contract_bytecode_hex = hex(int(contract_bytecode, 16))
+
+    consent_contract_factory = w3.eth.contract(address=address_contract_factory, abi=contract_abi,
+                                               bytecode=contract_bytecode_hex)
+
+    create_consent_contract_tx = consent_contract_factory.functions.createConsentContract("12345").build_transaction(
         {
             'from': GOQUORUM_NODE.accountAddress,
             'nonce': w3.eth.get_transaction_count(GOQUORUM_NODE.accountAddress),
@@ -118,17 +140,25 @@ def create_consent_contract(address_consenter: str = typer.Option(..., "--adr-co
     # sign and make createContract transaction
     make_contract_tx(create_consent_contract_tx)
 
-    # get deployed consentContract address
-    deployed_contract_address = consent_contract_factory.functions.getContractAddress().call()
-
     # read base contract abi
     contract_abi_read = os.popen(TRUFFLE_READ_CONTRACT_ABI)
     contract_abi = json.loads(contract_abi_read.read())["abi"]
 
-    consent_contract = w3.eth.contract(address=deployed_contract_address, abi=contract_abi, bytecode=contract_bytecode_hex)
+    contract_bytecode_read = os.popen(TRUFFLE_CONTRACT_BYTECODE)
+    contract_bytecode = contract_bytecode_read.read().strip()
+    contract_bytecode_hex = hex(int(contract_bytecode, 16))
+
+    deployed_contract_address = consent_contract_factory.functions.getContractAddress("12345").call()
+
+    consent_contract = w3.eth.contract(address=deployed_contract_address, abi=contract_abi,
+                                       bytecode=contract_bytecode_hex)
+
+    # build checkSum from consenter's address
+    checksum_address_consenter = w3.toChecksumAddress(address_consenter)
 
     # set consenter address
-    set_consenter_tx = consent_contract.functions.setConsenter(checksum_address_consenter).build_transaction(
+    set_contract_details_tx = consent_contract.functions.setContractDetails(checksum_address_consenter,
+                                                                            1, 1, 1, 1, 1).build_transaction(
         {
             'from': GOQUORUM_NODE.accountAddress,
             'nonce': w3.eth.get_transaction_count(GOQUORUM_NODE.accountAddress),
@@ -136,10 +166,57 @@ def create_consent_contract(address_consenter: str = typer.Option(..., "--adr-co
         }
     )
 
-    # sign and make setConsenter transaction
-    make_contract_tx(set_consenter_tx)
+    # sign and make setContractDetails transaction
+    make_contract_tx(set_contract_details_tx)
 
-    print(f'deployed contractFactory {deployed_contract_factory_address} and deployed contract {deployed_contract_address}')
+    # sign the contract
+    sign_contract_tx = consent_contract.functions.signContract().build_transaction(
+        {
+            'from': GOQUORUM_NODE.accountAddress,
+            'nonce': w3.eth.get_transaction_count(GOQUORUM_NODE.accountAddress),
+            'gasPrice': w3.eth.gas_price
+        }
+    )
+
+    # sign and make signContract transaction
+    make_contract_tx(sign_contract_tx)
+    print(f'deployedAddress (factory): {deployed_contract_address}')
+
+
+@cli.command()
+def deposit_to_contract(
+        address_contract: str = typer.Option(..., "--adr-con", "-ac", help="address of the contract"),
+        value: int = typer.Option(..., "--val", "-v", help="value in ETH")
+):
+    if not w3.isConnected():
+        print("Ethereum Node not running...")
+        return
+
+    # read base contract abi
+    contract_abi_read = os.popen(TRUFFLE_READ_CONTRACT_ABI)
+    contract_abi = json.loads(contract_abi_read.read())["abi"]
+
+    contract_bytecode_read = os.popen(TRUFFLE_CONTRACT_BYTECODE)
+    contract_bytecode = contract_bytecode_read.read().strip()
+    contract_bytecode_hex = hex(int(contract_bytecode, 16))
+
+    consent_contract = w3.eth.contract(address=address_contract, abi=contract_abi,
+                                       bytecode=contract_bytecode_hex)
+
+    # sign the contract
+    sign_contract_tx = consent_contract.functions.deposit().build_transaction(
+        {
+            'from': GOQUORUM_NODE.accountAddress,
+            'nonce': w3.eth.get_transaction_count(GOQUORUM_NODE.accountAddress),
+            'gasPrice': w3.eth.gas_price,
+            'value': value
+        }
+    )
+
+    # sign and make signContract transaction
+    make_contract_tx(sign_contract_tx)
+
+    print(f'balance of contract: {get_account_balance(str(address_contract), True)}')
 
 
 @cli.command()
@@ -153,19 +230,29 @@ def account_unlock():
 def account_balance():
     """returns the current balance of the ethereum account in ETH"""
 
+    return get_account_balance(GOQUORUM_NODE.accountAddress, False)
+
+
+def get_account_balance(address, no_convert: bool):
     if not w3.isConnected():
         print("Ethereum Node not running...")
         return
 
-    account_balance_read = os.popen(GOQUORUM_ACCOUNT_BALANCE % (GOQUORUM_NODE.accountAddress, GOQUORUM_NODE_URL))
+    account_balance_read = os.popen(GOQUORUM_ACCOUNT_BALANCE % (address, GOQUORUM_NODE_URL))
     account_balance = json.loads(account_balance_read.read())
 
     if "error" in account_balance:
         print("ERROR: check configuration")
         return
     elif account_balance["result"]:
-        balance = w3.fromWei(literal_eval(account_balance["result"]), 'ether')
+        balance = 0
+        if no_convert:
+            balance = account_balance["result"]
+        else:
+            balance = w3.fromWei(literal_eval(account_balance["result"]), 'ether')
+
         print(balance)
+        return balance
 
 
 def get_account_details():
@@ -190,7 +277,8 @@ def get_account_details():
 
 def unlock_account(node):
     """temporaly unlocks the local ethereum account"""
-    account_unlock_read = os.popen(GOQUORUM_ACCOUNT_UNLOCK % (node.accountAddress, node.accountPassword, GOQUORUM_NODE_URL))
+    account_unlock_read = os.popen(
+        GOQUORUM_ACCOUNT_UNLOCK % (node.accountAddress, node.accountPassword, GOQUORUM_NODE_URL))
     account_unlock = json.loads(account_unlock_read.read())
 
     if "error" in account_unlock:
@@ -207,6 +295,7 @@ def make_contract_tx(tx):
     tx_create = w3.eth.account.sign_transaction(tx, GOQUORUM_NODE.accountPrivateKey)
     tx_hash = w3.eth.send_raw_transaction(tx_create.rawTransaction)
     tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+    print(tx_receipt['transactionHash'])
     return tx_receipt.contractAddress
 
 
